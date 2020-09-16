06908c4dc4a2e8806d6cc4851218d1476469179a
Valeriy <Valeriy>
2020-09-16 17:37:20 +0700

    Create storyboards, controllers, files structure

---
+++ AutoCatalogRealm/Controllers/CarsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarsTableViewCell.swift
+//  AutoCatalogRealm
+//
+//  Created by Valeriy Pokatilo on 16.09.2020.
+//  Copyright © 2020 Valeriy Pokatilo. All rights reserved.
+//
+
+import UIKit
+
+class CarsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
